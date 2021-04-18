1f82d82029a0f6f33b103f84694cf471be7110ca
sameh ayman <sameh.el3gamy520@gmail.com>
2021-04-18 15:36:18 +0200

    a structure representing student information is created

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct student
+{
+    int ID ;
+    int day_of_birth ;
+    int month_of_birth ;
+    int year_of_birth ;
+    int score;
+    char student_name[];
+};
+int main()
+{
+
+    return 0;
+}
